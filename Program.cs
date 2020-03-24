8a322a59d17d3f63d077b055d02994793accc8cd
Joseph Chatelin <josephchatelin@gmail.com>
2020-03-24 10:18:52 +0100

    iteration 2

--- Program.cs
+++ Program.cs
@@ -7,9 +7,13 @@ namespace kebab_project
     {
         static void Main(string[] args)
         {
-            List<String> ingredients = new List<String>{"Pain", "Salade", "Tomate"};
-            Kebab myKebab = new Kebab(ingredients);
-            Console.WriteLine("is vegi : " + myKebab.isVegetarian());
+            List<String> ingredients = new List<String>{"Pain", "Salade", "Tomate", "Oignons"};
+            Kebab myKebabVegi = new Kebab(ingredients);
+            Console.WriteLine("is vegi : " + myKebabVegi.isVegetarian());
+            ingredients.Add("Crevette");
+            Kebab myKebabPesce = new Kebab(ingredients);
+            Console.WriteLine("is pesce : " + myKebabPesce.isVegetarian());
+            
         }
     }
 
@@ -21,10 +25,19 @@ namespace kebab_project
         }
 
         public bool isVegetarian() {
-            if(ingredients.Contains("Poulet") || ingredients.Contains("Viande")) {
+            if(ingredients.Contains("Poulet") || ingredients.Contains("Viande") 
+            || ingredients.Contains("Poisson") || ingredients.Contains("Crevettes")) {
                 return false;
             } else {
                 return true;
+            }
+        }
+
+        public bool isPescotarian() {
+            if(ingredients.Contains("Poisson") || ingredients.Contains("Crevettes")) {
+                return true;
+            } else {
+                return false;
             }
         }
     }
