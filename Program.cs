78d02a925380d914ddbcb66653033da116659a95
Joseph Chatelin <josephchatelin@gmail.com>
2020-03-24 10:47:08 +0100

    iteration 4

--- Program.cs
+++ Program.cs
@@ -7,9 +7,14 @@ namespace kebab_project
     {
         static void Main(string[] args)
         {
-            List<String> ingredients = new List<String>{"Pain", "Salade", "Tomate", "Oignons"};
-            Kebab myKebab = new Kebab(ingredients);
-            Console.WriteLine("is vegi : " + myKebab.isVegetarian());
+
+            List<String> ingredients = new List<String>{"Pain", "Salade", "Tomate", "Oignons", "Fromage"};
+            Kebab myKebabVegi = new Kebab(ingredients);
+            Console.WriteLine("is vegi : " + myKebabVegi.isVegetarian());
+            Kebab myKebabOnions = new Kebab(ingredients);
+            Console.WriteLine("is withoutOnions : " + myKebabOnions.withOutOnions().getIngredients().Count);
+            Kebab myKebabCheese = new Kebab(ingredients);
+            Console.WriteLine("is suppCheese : " + myKebabCheese.supplementCheese().getIngredients().Count);
         }
     }
 
@@ -18,6 +23,10 @@ namespace kebab_project
         private List<String> ingredients;
         public Kebab(List<String> ingredients) {
             this.ingredients = ingredients;
+        }
+
+        public List<String> getIngredients() {
+            return ingredients;
         }
 
         public bool isVegetarian() {
@@ -43,5 +52,32 @@ namespace kebab_project
             }
         }
 
+        public Kebab withOutOnions() {
+            if(ingredients.Contains("Oignons")) {
+                ingredients.Remove("Oignons");
+            }
+            return this;
+        }
+
+        private bool isCheese(String ingredient) {
+            if(ingredient == "Fromage") {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        public Kebab supplementCheese() {
+            Predicate<String> cheese = new Predicate<string>(isCheese);
+            int cheeseCount = 0;
+            if(ingredients.Contains("Fromage")) {
+                cheeseCount = ingredients.FindAll(cheese).Count;
+                for(int i = 0; i < 2*cheeseCount; i++) {
+                    ingredients.Add("Fromage");
+                }
+            } else {
+                ingredients.Add("Fromage");    
+            }
+            return this;
+        }
     }
 }
